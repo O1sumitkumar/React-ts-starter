9fa15c5bdfb51b049ef8f4eca5d1a514892e8409
O1sumitkumar <SUMITJHA365@gmail.com>
2024-02-11 21:26:20 +0530

    df

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-
+const base = '/vertical/';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? base : '/',
   build: {
     outDir: 'dist', // Specify the output directory
   },
