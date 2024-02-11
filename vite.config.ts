6f9e9b6075260e34e0177d226344ee8b62186479
O1sumitkumar <SUMITJHA365@gmail.com>
2024-02-11 21:18:49 +0530

    d

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // Specify the output directory
+    outDir: 'dist', // Specify the output directory
   },
 });
